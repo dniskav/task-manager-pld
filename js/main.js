2c83e315bd20b69a4483e4b99799a09bda0345bd
Alejandro Nanez <alejandro@alejandronanez.com>
2013-02-12 16:27:51 -0500

    Created basic functionality.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
 // Global object
 window.taskManager = {};
+
+function Task() {
+	this.name = 'Task name';
+	this.priority = 5;
+	this.description = 'This is the task description';
+	this.user = 'Dummy user';
+	this.state = 'Open';
+	// DD/MM/YYYY
+	this.due_date = '01/03/2013';
+
+	/**
+	 * Adding a new task
+	 */
+	this.addTask = function() {
+
+	};
+
+	/**
+	 * Delete a task
+	 */
+	this.deleteTask = function() {
+
+	};
+
+	/**
+	 * Set a task a resolved
+	 */
+	this.resolveTask = function() {
+
+	};
+};
+
+var tarea = new Task();
