67a4c3acfca0c5efc46f2feef8594bdd0dcc662e
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-02-12 17:09:44 -0500

    base class structure

--- js/main.js
+++ js/main.js
@@ -1,2 +1,51 @@
 // Global object
-window.taskManager = {};
+window.taskManager = {
+    
+    Task : {
+        
+        tUser: {
+            id : 0, 
+            name : "lorem"
+        },
+        
+        
+        //get & sets
+        getUser : function(){
+        
+            console.info(this);
+        
+        },
+    
+        setUser : function(user){
+        
+            this.tUser.name = user;
+        
+        },
+        //other methodos
+        
+        /**
+         *  add an unser to  a task
+         *  return boolean
+         */
+        addUser : function(){
+        
+            console.info(this);
+            
+            return true;
+        
+        }, 
+        /**
+         * edit user in a task
+         * return boolean
+         */
+        editUser : function(){
+            
+            console.info("edit user: " + user);
+            
+            return true
+        }
+        
+    }
+};
+
+
