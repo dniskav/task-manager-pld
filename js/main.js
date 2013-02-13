2273cac321c5c063ef9595cd4b494f7688ee7a76
Alejandro Nanez <alejandro@alejandronanez.com>
2013-02-13 08:14:08 -0500

    Start using namespacing.

--- js/main.js
+++ js/main.js
@@ -1,62 +1,68 @@
-// Global object
-window.taskManager = {
+// Check if the TASKMANAGER namespace exists
+var TASKMANAGER = TASKMANAGER || {};
 
-	'name' : 'Task name',
-	'priority' : 5,
-	'description' : 'This is the task description',
-	'user' : 'Dummy user',
-	'state' : 'Open',
-	// DD/MM/YYYY
-	'due_date' : '01/03/2013',
+TASKMANAGER = {
 
-	/**
-	 * Set default parameters reading from JSON file.
-	 */
-	initialize: function(taskOptions) {
+    tasksArray : [],
 
-		for ( var property in taskOptions ) {
+    Task : {
+        'id' : 1,
+        'name' : 'Task name',
+        'priority' : 5,
+        'description' : 'This is the task description',
+        'user' : 'Dummy user',
+        'state' : 'Open',
+        'due_date' : '01/03/2013', // DD/MM/YYYY
 
-			if ( !taskOptions.hasOwnProperty(property) ) {
-				console.log("Property " + property + " is invalid");
-				console.log( taskOptions[property] );
-			} else {
-				console.log( taskOptions[property] );
-			}
-			
-		}
-	},
+        /**
+         * Adding a new task
+         */
+        addTask: function (taskOptions) {
+            "use strict";
+            if (!taskOptions) {
+                alert('Please set parameters');
+            }
 
-	/**
-	 * Adding a new task
-	 */
-	addTask: function() {
+            TASKMANAGER.tasksArray.push(taskOptions);
+        },
 
-	},
+        /**
+         * Delete an specific task.
+         * @param  {Object} task Task to delete
+         */
+        deleteTask: function (task) {
+            "use strict";
+            
+            console.log("Task deleted");
+        },
 
-	/**
-	 * Delete a task
-	 */
-	deleteTask: function() {
+        /**
+         * Set a task a resolved
+         */
+        resolveTask: function () {
+            "use strict";
 
-	},
-
-	/**
-	 * Set a task a resolved
-	 */
-	resolveTask: function() {
-
-	}
+        }
+    }
 };
 
+// Dummy object. Just for testing
+var userObject = {
+    'name' : 'Alejandro',
+    'last_name' : 'Nanez Ortiz',
+    'age' : 26,
+    'position' : 'Frontend Developer'
+};
+
+// Dummy object. Just for testing
 var taskOptions = {
-	'dummything' : 'dasdasda',
-	'name' : 'Finish my tutorial',
-	'priority' : 10,
-	'description' : 'I must finish my tasks!',
-	'user' : 'Alejandro Nanez Ortiz',
-	'state' : 'Open',
-	// DD/MM/YYYY
-	'due_date' : '01/03/2013',
+    'name' : 'Finish my tutorial',
+    'priority' : 10,
+    'description' : 'I must finish my tasks!',
+    'user' : userObject,
+    'state' : 'Open',
+    'due_date' : '01/03/2013' // DD/MM/YYYY
 };
 
-taskManager.initialize(taskOptions);
+// Initialize myTask
+var myTask = new TASKMANAGER.Task.addTask(taskOptions);
