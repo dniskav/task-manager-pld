4d310eb9fea37bf121110ea2b6bd489d9f74f5c4
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-02-13 11:52:07 -0500

    first stage for add user, and adducer to task

--- js/main.js
+++ js/main.js
@@ -1,50 +1,134 @@
 // Global object
 window.taskManager = {
-    
-    Task : {
+    ids: 0,
+    tUsers: [],
+    container: null,
+    /**
+     *
+     */
+    init: function(jsonData) {
+        taskManager.container = jsonData.container;
+    },
+    render: {
+    /**
+     *
+     */
+        user: function(user) {
         
-        tUser: {
-            id : 0, 
-            name : "lorem"
-        },
-        
-        
+            taskManager.container.appendChild(taskManager.templates.user(user));
+
+        }
+    },
+    templates : {
+    /**
+     *
+     */
+        user : function(user){
+            usrData = user;
+            
+            tmpl = document.createElement('div');
+            tmpl.id = 'user-' + usrData.id;
+            tmpl.className = 'user-pipe';
+            tmpl.appendChild(
+                document.createTextNode(usrData.name)
+            );
+            
+            return tmpl;
+        }
+    },
+    task: {
         //get & sets
-        getUser : function(){
-        
-            console.info(this);
-        
+    /**
+     *
+     */
+        getUser: function(id) {
+
+            return taskManager.tUsers[id];
+
         },
-    
-        setUser : function(user){
-        
-            this.tUser.name = user;
-        
+    /**
+     *
+     */
+        setUser: function(user) {
+
         },
         //other methodos
-        
+
         /**
          *  add an unser to  a task
-         *  return boolean
+         *  @param {integer} int
+         *  @return boolean
          */
-        addUser : function(){
-        
-            console.info(this);
-            
-            return true;
-        
-        }, 
+        addUser: function(user) {
+
+        },
         /**
          * edit user in a task
-         * return boolean
+         * @return boolean
          */
-        editUser : function(){
-            
+        editUser: function() {
+
             console.info("edit user: " + user);
-            
+
             return true
         }
+
+    },
+    user:{
         
+        
+        /**
+         * add user to scope
+         * @param {integer} user
+         * @return {integer} id user
+         */
+        setUser : function(user){
+            current_id = taskManager.ids;
+
+            objUser = {
+                id: current_id,
+                name: user
+            }
+
+            taskManager.tUsers[current_id] = objUser;
+
+            taskManager.ids++;
+
+            if (typeof taskManager.tUsers[current_id] != 'undefined') {
+
+                return current_id;
+            } else {
+                return false;
+            }
+        },
+        /**
+         *get user from users array
+         *@param {integer} id
+         *@return {object} user
+         **/
+        getUser : function(id){
+            return taskManager.tUsers[id];
+        },
+        /**
+         * add user to a enviroment
+         * @param {integer} user
+         * @return {bolean}
+         */
+        add : function(user){
+            
+            id_User = this.setUser(user);
+
+            usr_data = this.getUser(id_User);
+            
+            if(typeof usr_data != 'undefined'){
+                taskManager.render.user(usr_data);
+            }
+
+            console.info(usr_data);
+
+            return true;
+
+        }
     }
 };
 
